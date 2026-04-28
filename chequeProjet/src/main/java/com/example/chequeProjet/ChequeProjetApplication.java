a7b87ddbd4a89873f7b0f398d3870c9607b293a0
AndriantsehenoAnja <andriantsehenoanja926@gmail.com>
2026-04-28 11:30:29 +0300

    first commit

---
+++ chequeProjet/src/main/java/com/example/chequeProjet/ChequeProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.chequeProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequeProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChequeProjetApplication.class, args);
+	}
+
+}
---
+++ chequeProjet/src/test/java/com/example/chequeProjet/ChequeProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chequeProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequeProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
